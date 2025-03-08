db5c2aba89ea5a47f38b4587a2b5cbf2890a4d00
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-03-08 15:13:29 +0300

    :shirt: refactor: refactor RegisterPage

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,40 +2,32 @@ import RegisterForm from "@/components/RegisterForm";
 import { Toaster, toaster } from "@/components/ui/toaster";
 import { RegisterRequest } from "@/models";
 import { useRegister } from "@/usecases/useRegister";
-import { useEffect } from "react";
 
 const RegisterPage = () => {
     const {
         mutate,
-        isPending,
-        isError,
-        isSuccess
+        isPending
     } = useRegister();
 
     const handleRegister = (registerRequest: RegisterRequest) => {
         console.log('register request', registerRequest);
-        mutate(registerRequest);
+        mutate(registerRequest, {
+            onSuccess: () => {
+                toaster.create({
+                    type: "success",
+                    title: "Аккаунт создан",
+                    description: "Войдите в приложение"
+                });
+            },
+            onError: () => {
+                toaster.create({
+                    type: "error",
+                    title: "Ошибка",
+                    description: "Пожалуйста, повторите попытку позже"
+                });
+            }
+        });
     };
-
-    useEffect(() => {
-        if (isError) {
-            toaster.create({
-                type: "error",
-                title: "Ошибка",
-                description: "Пожалуйста, повторите попытку позже"
-            });
-        }
-    }, [isError]);
-    
-    useEffect(() => {
-        if (isSuccess) {
-            toaster.create({
-                type: "success",
-                title: "Аккаунт создан",
-                description: "Войдите в приложение"
-            });
-        }
-    }, [isSuccess]);
 
     return (
         <>
